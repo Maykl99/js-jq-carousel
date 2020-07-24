079d4561329d05b4cc5d6f4198b971f3cab10685
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-24 09:07:57 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -7,10 +7,9 @@ $(document).ready(function(){
         $(this).addClass('active');
         
         
-        var click=$(this).index(); //individuo l'index del pallino cliccato
+        var click=$(this).index(); //individuo l'indice/index del pallino cliccato
         console.log(click);
         var img=$('.img-container img').eq(click);  // individuo l'immagine con lo stesso index del pallino cliccato
-        //console.log(img)
         $('.img-container img.active').removeClass('active');
         img.addClass('active');
 
@@ -27,18 +26,24 @@ $(document).ready(function(){
 });
 
 
+
 // funzioni
+//-> prossima immagine +1
 function nextImg(){
+
     var activeImg=$('.img-container .active');
 
     var cerchioAttivo=$('.nav i.active');
     cerchioAttivo.removeClass('active');
 
     if(activeImg.hasClass('close')){
+
         activeImg.removeClass('active');
         $('.img-container .first').addClass('active');
         $('.nav i.first').addClass('active');
+
     }else{
+
         activeImg.next().addClass('active');
         activeImg.removeClass('active').next().addClass('active');
         cerchioAttivo.next().addClass('active');
@@ -47,17 +52,22 @@ function nextImg(){
     
 }
 
+//-> immagine prima -1
 function prevImg(){ 
+
     var activeImg=$('.img-container .active');
 
     var cerchioAttivo=$('.nav i.active');
     cerchioAttivo.removeClass('active');
 
     if(activeImg.hasClass('first')){
+
         activeImg.removeClass('active');
         $('.img-container img.close').addClass('active');
         $('.nav i.last').addClass('active');
+
     }else{
+
         activeImg.prev().addClass('active');
         activeImg.removeClass('active').prev().addClass('active');
         cerchioAttivo.prev().addClass('active');
